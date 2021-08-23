56a4ba7847db3ff4ac92423856105f7c261613ea
Irfa Sheikh <78042313+irfasheikh@users.noreply.github.com>
2021-08-23 12:01:24 -0500

    Update lifeinweeks.js

---
+++ Javascript/lifeinweeks.js
@@ -0,0 +1,9 @@
+//how many days ,weeks, months until we are 90 years old
+
+function lifeInWeeks (age){
+
+  var yearsLeft = 90 - age;
+  var weeksLeft = yearsLeft * 52;
+  var monthsLeft = weeksLeft * 12;
+  var daysLeft = monthsLeft * 365;
+}
